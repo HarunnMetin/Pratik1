2f4325db1510b8215d599a992b793c237941f38e
HarunnMetin <mtnmusa07@gmail.com>
2024-04-14 23:48:34 +0300

    Initial commit

---
+++ src/test/java/Tests/Tekrar1/Day1/C01_IlkTest.java
@@ -0,0 +1,16 @@
+package Tests.Tekrar1.Day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_IlkTest {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\acer\\Pratik1\\KurulumDosyalari\\chromedriver-win64 (3)\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.com");
+    }
+}
